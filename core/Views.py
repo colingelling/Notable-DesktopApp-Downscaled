f6f1e39acf37cf8c952c71368e30f2512782b027
Colin Gelling <22787309+colingelling@users.noreply.github.com>
2024-08-15 11:51:54 +0200

    Also removed ui file definitions from Views -functions because those will be reserved for what's going to be added to windows

--- core/Views.py
+++ core/Views.py
@@ -14,25 +14,19 @@ class Views:
 		pass
 
 	def options(self):
-		qt_creator_file = "/src/ui/CreatorOptionsDialog.ui"
 		pass
 
 	def add_notebook(self):
-		qt_creator_file = "/src/ui/CreateNotebookDialog.ui"
 		pass
 
 	def delete_notebook(self):
-		qt_creator_file = "/src/ui/DeleteNotebookDialog.ui"
 		pass
 
 	def add_note(self):
-		qt_creator_file = "/src/ui/CreateNoteDialog.ui"
 		pass
 
 	def edit_note(self):
-		qt_creator_file = "/src/ui/EditNoteDialog.ui"
 		pass
 
 	def delete_note(self):
-		qt_creator_file = "/src/ui/DeleteNoteDialog.ui"
 		pass
